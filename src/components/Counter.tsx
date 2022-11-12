ab76ad406a37d32c4a1587b0b18c85f06e612de4
v_xddyang <1552614119@qq.com>
2022-11-12 22:29:02 +0800

    update

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,35 @@
+
+
 import { createSignal } from 'solid-js';
 
 export function Counter (props) {
-    const [count, setCount] = createSignal(props.count)
-
+    const [ count, setCount ] = createSignal(props.count);
+    console.log('qqq', createSignal(props.count));
     return (
         <div>
-            yangxiaodong {count}
+            冯晓婷{count()}
 
-            <button onClick = {() => setCount(count() + 1)}>click</button>
+            <button onclick = { () => setCount(count() + 1) }>+1</button>
         </div>
     )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
